478e66cca8d1937a4a05cf36d3b9dd6563e2bf58
antunesdvz <g.2008.antunes@gmail.com>
2025-08-19 22:30:48 -0300

    Alteração Panda Radio 2

--- src/app/services/panda_radio.service.ts
+++ src/app/services/panda_radio.service.ts
@@ -9,9 +9,22 @@ export class PandaRadioService {
   constructor() {}
 
   async obterUrlLogin(): Promise<string> {
-
+    const codigoAleatorio = '';
     const authPoint = `${ThePandaConfiguration.authEndpoint}?`;
     const clientId = `client_id=${ThePandaConfiguration.clientId}&`;
     const urlRedirect = `redirect_uri=${ThePandaConfiguration.redirectUrl}&`;
+    const scopes = `scopes=${ThePandaConfiguration.scopes.join('%20')}`;
+    const codeChallengeMethod = 'code_challenge_method=S256';
+    const codeChallengeParam = 'code_challenge' + codigoAleatorio + '&';
+    const responseType = 'response_type=code';
+
+    return `${authPoint}${clientId}${urlRedirect}${scopes}${codeChallengeMethod}${codeChallengeParam}${responseType}`;
+  }
+
+  async gerarCodigoAleatorio(): Promise<any> {
+    const codigoVerificador = this.gerarCodigoAleatorio(128);
+    const codchallenge = await this.gerarHash(codigoVerificador);
+
+    return codchallenge;
   }
 }
